60a568b7c6cf9ed297df3247f4d304e2fb20b848
Julija Katukova <julija.katukova@gmail.com>
2021-07-12 19:18:38 +0300

    correction

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,14 +3,14 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Circle circle1 = new Circle(5);
+        Circle circle1 = new Circle(14);
         System.out.println(circle1.findCircumference());
         System.out.println(circle1.findArea());
 
         Circle circle2 = new Circle();
         System.out.println(circle2.findCircumference());
         System.out.println(circle2.findArea());
-        circle2.setRadius(3);
+        circle2.setRadius(7);
         System.out.println(circle2.findCircumference());
         System.out.println(circle2.findArea());
     }
